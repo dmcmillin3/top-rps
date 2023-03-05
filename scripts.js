8b44af0b8acbbb94cdf39167f05fb2e47fc79837
Devon <demcmillin@gmail.com>
2023-03-05 17:50:39 -0500

    Add primary game function
    Add playRockPaperScissors() to allow for single rounds of
    the game to be played. Hard coded a user guess to simulate
    a round being played.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let computerChoice = "";
+let playerChoice = "Scissors";
 
 function getComputerChoice() {
     let randomNumber = Math.random();
@@ -18,3 +19,29 @@ function getComputerChoice() {
 
 getComputerChoice();
 console.log(computerChoice);
+
+function playRockPaperScissors(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        return console.log("You both chose " + playerSelection.toLowerCase() + ", it is a tie.")
+    } else if (playerSelection.toLowerCase() == "rock") {
+        if (computerSelection.toLowerCase() == "paper") {
+            return console.log("You lose! Paper beats rock.")
+        } else {
+            return console.log("You win! Rock beats scissors.")
+        }
+    } else if (playerSelection.toLowerCase() == "paper") {
+        if (computerSelection.toLowerCase() == "scissors") {
+            return console.log("You lose! Scissors beats paper.")
+        } else {
+            return console.log("You win! Paper beats rock.")
+        }
+    } else {
+        if (computerSelection.toLowerCase() == "rock") {
+            return console.log("You lose! Rock beats scissors.")
+        } else {
+            return console.log("You win! Scissors beats paper.")
+        }
+    }
+}
+
+playRockPaperScissors(playerChoice, computerChoice);
