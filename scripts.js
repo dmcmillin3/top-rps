88be661104a2af22e20c5475408953e3bbca6e42
Devon <demcmillin@gmail.com>
2023-03-05 19:32:42 -0500

    Add game loop function
    Add primary game loop function to allow multiple rounds.
    Further adjusted variables and functions to accommodate
    the point system as well as changing user and computer
    choices. Removed console.log() debug outputs and cleaned
    up the console outputs during the game.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
-const computerChoice = getComputerChoice();
-const playerChoice = "Scissors";
+let computerChoice = "";
+let playerChoice = "";
+let computerScore = 0;
+let playerScore = 0;
 
 function getComputerChoice() {
     let randomNumber = Math.random();
-    console.log("Random Number: " + randomNumber);
 
     if (randomNumber >= 0.67) {
         return "Rock"
@@ -19,24 +20,48 @@ function playRockPaperScissors(playerSelection, computerSelection) {
         return "You both chose " + playerSelection.toLowerCase() + ", it is a tie."
     } else if (playerSelection.toLowerCase() == "rock") {
         if (computerSelection.toLowerCase() == "paper") {
+            computerScore++;
             return "You lose! Paper beats rock."
         } else {
+            playerScore++;
             return "You win! Rock beats scissors."
         }
     } else if (playerSelection.toLowerCase() == "paper") {
         if (computerSelection.toLowerCase() == "scissors") {
+            computerScore++;
             return "You lose! Scissors beats paper."
         } else {
+            playerScore++;
             return "You win! Paper beats rock."
         }
     } else {
         if (computerSelection.toLowerCase() == "rock") {
+            computerScore++;
             return "You lose! Rock beats scissors."
         } else {
+            playerScore++;
             return "You win! Scissors beats paper."
         }
     }
 }
 
-console.log(computerChoice);
-console.log(playRockPaperScissors(playerChoice, computerChoice));
+function game() {
+    computerScore, playerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        computerChoice = getComputerChoice();
+        playerChoice = prompt("Rock, Paper, or Scissors?: ");
+        console.log(playRockPaperScissors(playerChoice, computerChoice));
+        console.log("Player score: " + playerScore);
+        console.log("Computer score: " + computerScore);
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations, you've won the game!");
+    } else if (computerScore > playerScore) {
+        console.log("Sorry, you lost to the computer.");
+    } else {
+        console.log("The game was a tie.");
+    }
+}
+
+game();
